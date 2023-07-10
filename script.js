adf18918de8e60dae69a9df9a832c6fd36a8a4a6
HRISHIKESH TIWARI <hrishikeshtiwari@HRISHIKESHs-MacBook-Air.local>
2023-07-10 19:10:43 +0530

    Week1 submission

---
+++ script.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("demobutton");
+
+button.addEventListener("click" , function(){
+    alert("You Clicked the button") ;
+    button.style.backgroundColor = "red";
+}) 
